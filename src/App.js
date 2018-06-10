1cc3ca8f8b213b9424a801053ee1bc6f930f344b
github username <github email address>
2018-06-10 09:42:36 -0400

    01_practice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CommentBox from './containers/CommentBox'
+import { Route, Switch, withRouter } from 'react-router-dom'
+import LoginForm from './components/LoginForm'
+import SignupForm from './components/SignupForm'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends React.Component{
+	state = {
+		currentUser: {username: "Anonymous"}
+	}
+
+	login = (userInput, type) => {
+		let headers = {
+			"Content-Type": 'application/json',
+			"Accept": "application/json"
+		}
+		fetch(`http://localhost:3001/api/${type}`, {
+			method: "POST",
+			headers: headers,
+			body: JSON.stringify(userInput)
+		})
+		.then(res => res.json())
+		.then(res => {
+			if (res.error){
+				alert(res.error)
+			} else {
+				this.setState({currentUser: res}, this.props.history.push("/comments"))
+			}
+		})
+	}
+
+	render(){
+		return (
+			<Switch>
+				<Route path="/comments" render={(props) => <CommentBox {...props} currentUser={this.state.currentUser}/>}/>
+				<Route path="/login" render={(props) => <LoginForm {...props} login={this.login}/>}/>
+				<Route path="/signup" render={(props) => <SignupForm {...props} login={this.login}/>}/>
+			</Switch>
+		)
+	}
 }
 
-export default App;
+export default withRouter(App)
---
+++ src/data/data.js
@@ -0,0 +1,25 @@
+const DATA = [
+	{
+		_id: 1,
+		author: "Steven",
+		text: "Dude, the hell?"
+	},
+	{
+		_id: 2,
+		author: "Steven",
+		text: "Gross"
+	},
+	{
+		_id: 3,
+		author: "Julie",
+		text: "Yeah gross"
+	},
+	{
+		_id: 4,
+		author: "Albert",
+		text: "Yum"
+	},
+
+]
+
+export default DATA
